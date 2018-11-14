70b287c12346a454ed7c7bcf093586a098970c7d
Austin Henriksen <austin.r.henriksen79@gmail.com>
2018-11-14 01:08:50 -0500

    Fixed bound checking in piecewise functions.

--- Force.cpp
+++ Force.cpp
@@ -73,7 +73,7 @@ namespace force
         int i = 0;
         for(; i < bounds.size(); i++)
         {
-            if(bounds[i] <= position)
+            if(bounds[i] >= position)
             {
                 if((bounds[i] == position) && (directions[i]))
                 {
